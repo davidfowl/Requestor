f651acbf473ce44d56da9d3c35cf7eaccf852d88
David Fowler <davidfowl@gmail.com>
2013-07-22 10:43:09 -0700

    Added portable lib

---
+++ GetRequest/Requestor.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetRequest
+{
+    public class Requestor
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        public async Task<T> Get<T>(string resource)
+        {
+            HttpResponseMessage response = await _client.GetAsync(resource);
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(content);
+        }
+    }
+}
